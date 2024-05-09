eb0a8a4f613ba6269ffb3223b0138bfef1020639
EmilioTec10 <110855627+EmilioTec10@users.noreply.github.com>
2024-05-09 17:03:20 -0600

    Se agrego el codigo conectdo de la ALU y el BCD

---
+++ Proyecto2/Slave/Slave_SPI.sv
@@ -0,0 +1,23 @@
+module Slave_SPI(
+	input logic CS,
+	input logic MOSI,
+	input logic SLCK,
+	output logic MISO,
+	output reg [6:0] display,
+	output logic Z, 
+	output logic C, 
+	output logic V, 
+	output logic N
+	);
+	
+	reg [3:0] A = 4'b0001;
+	reg [3:0] B = 4'b0011;
+	reg [1:0] op = 2'b00;
+	
+	reg [3:0] result;
+	
+	ALU alu1 (.A(A), .B(B), .opcode(op), .result(result), .N(N), .V(V), .Z(Z), .C(C));
+	
+	bin_to_bcd bdc1(.A(result[3]), .B(result[2]), .C(result[1]), .D(result[0]), .display(display));
+
+endmodule 
